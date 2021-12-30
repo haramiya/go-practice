c2fc558002cf2949394b3731577b45cf0a2421ed
Miyahara <haramiya21@gmail.com>
2021-12-30 11:29:48 +0900

    add: labelled for statement

---
+++ loop.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+Loop:
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			if j > 1 {
+				continue Loop
+			}
+			fmt.Println(i, j, i*j)
+		}
+	}
+}
